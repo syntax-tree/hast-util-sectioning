5239bd54f1749b51ef56d3d485992847ceb0e28e
Titus Wormer <tituswormer@gmail.com>
2021-04-26 12:48:49 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * @typedef {import('hast').Element & {tagName: 'article'|'aside'|'nav'|'section'}} Sectioning
+ * @typedef {import('hast-util-is-element').AssertPredicate<Sectioning>} AssertSectioning
+ */
+
 import {convertElement} from 'hast-util-is-element'
 
+/**
+ * Check if a node is a sectioning element.
+ * @type {AssertSectioning}
+ */
+// @ts-ignore Sure, the assertion matches.
 export const sectioning = convertElement(['article', 'aside', 'nav', 'section'])
